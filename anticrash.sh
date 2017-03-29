ab631e5bcfb2504377695282ab4c0d9b94ce3aae
SayedSadat <Shafiq_Sadat@hotmail.com>
2017-03-29 21:47:46 +0430

    Update anticrash.sh

--- anticrash.sh
+++ anticrash.sh
@@ -3,15 +3,16 @@ while true ; do
   for entr in tabchi-*.sh ; do
     entry="${entr/.sh/}"
     tmux kill-session -t $entry
-    rm -rf ~/.telegram-cli/$entry/data/animation/*
-    rm -rf ~/.telegram-cli/$entry/data/audio/*
-    rm -rf ~/.telegram-cli/$entry/data/document/*
-    rm -rf ~/.telegram-cli/$entry/data/photo/*
-    rm -rf ~/.telegram-cli/$entry/data/sticker/*
-    rm -rf ~/.telegram-cli/$entry/data/temp/*
-    rm -rf ~/.telegram-cli/$entry/data/video/*
-    rm -rf ~/.telegram-cli/$entry/data/voice/*
-    rm -rf ~/.telegram-cli/$entry/data/profile_photo/*
+    rm -rf ~/.telegram-cli/tabchi-*/data/animation/
+    rm -rf ~/.telegram-cli/tabchi-*/data/audio/
+    rm -rf ~/.telegram-cli/tabchi-*/data/document/
+    rm -rf ~/.telegram-cli/tabchi-*/data/photo/
+    rm -rf ~/.telegram-cli/tabchi-*/data/sticker/
+    rm -rf ~/.telegram-cli/tabchi-*/data/temp/
+    rm -rf ~/.telegram-cli/tabchi-*/data/video/
+    rm -rf ~/.telegram-cli/tabchi-*/data/voice/
+    rm -rf ~/.telegram-cli/tabchi-*/data/profile_photo/
+    rm -rf ~/.telegram-cli/tabchi-*/data/thumb/*.jpg
     tmux new-session -d -s $entry "./$entr"
     tmux detach -s $entry
   done
